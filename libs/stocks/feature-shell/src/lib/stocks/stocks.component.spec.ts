1c1c97f70bbea8254a048332a8b41c1fa957c45d
pallabita.panda <pallabita.panda5@t-mobile.com>
2020-01-23 23:46:35 +0530

    Changes for Task 1

--- libs/stocks/feature-shell/src/lib/stocks/stocks.component.spec.ts
+++ libs/stocks/feature-shell/src/lib/stocks/stocks.component.spec.ts
@@ -1,22 +1,34 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { StocksComponent } from './stocks.component';
+import { PriceQueryFacade } from '@coding-challenge/stocks/data-access-price-query';
+import { StocksFeatureShellModule } from '../stocks-feature-shell.module';
+import { of } from 'rxjs';
 
 describe('StocksComponent', () => {
   let component: StocksComponent;
   let fixture: ComponentFixture<StocksComponent>;
 
+  class MockPriceQueryFacade {
+    fetchQuote = jest.fn();
+  }
+
+  const testingModuleBase = {
+    providers: [
+      {
+        provide: PriceQueryFacade,
+        useClass: MockPriceQueryFacade
+      }
+    ],
+    imports: [StocksFeatureShellModule]
+  };
+
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ StocksComponent ]
-    })
-    .compileComponents();
+    TestBed.configureTestingModule(testingModuleBase).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(StocksComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
